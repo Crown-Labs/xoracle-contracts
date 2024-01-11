23bfb85688ec17504f866d26d4fcce49d959e15c
0xBishop <bishop@crown-labs.io>
2024-01-11 11:37:27 +0700

    chore: fix error message

--- contracts/xOracleMessage.sol
+++ contracts/xOracleMessage.sol
@@ -80,9 +80,9 @@ contract XOracleMessage is OwnableUpgradeable, PausableUpgradeable {
         require(!onlyWhitelist || whitelists[msg.sender], "whitelist: forbidden");
         
         // check params
-        require(_payload.length > 0, "payload invalid");
-        require(_endpoint != address(0), "endpoint invalid");
-        require(_dstChainId != chainId, "dstChainId invalid");
+        require(_payload.length > 0, "invalid payload");
+        require(_endpoint != address(0), "invalid endpoint");
+        require(_dstChainId != chainId, "invalid dstChainId");
 
         // request fee
         uint256 fee = getFee(_dstChainId);
@@ -120,10 +120,10 @@ contract XOracleMessage is OwnableUpgradeable, PausableUpgradeable {
         bytes32 _srcTxHash,
         bytes[] memory _signatures
     ) external onlyController {
-        require(_payload.length > 0, "payload invalid");
-        require(_endpoint != address(0), "endpoint invalid");
+        require(_payload.length > 0, "invalid payload");
+        require(_endpoint != address(0), "invalid endpoint ");
         require(_srcChainId != _dstChainId, "invalid chainId");
-        require(_dstChainId == chainId, "dstChainId invalid");
+        require(_dstChainId == chainId, "invalid dstChainId");
 
         bytes32 messageHash = getMessageHash(_nonce, _payload, _endpoint, _srcChainId, _dstChainId, _srcTxHash);
         require(fulfillMessageHashes[messageHash] == false, "messageHash already fulfilled");
@@ -197,13 +197,13 @@ contract XOracleMessage is OwnableUpgradeable, PausableUpgradeable {
     }
 
     function setController(address _controller, bool _flag) external onlyOwner {
-        require(_controller != address(0), "address invalid");
+        require(_controller != address(0), "invalid address");
         controller[_controller] = _flag;
         emit SetController(_controller, _flag);
     }
 
     function setSigner(address _signer, bool _flag) external onlyOwner {
-        require(_signer != address(0), "address invalid");
+        require(_signer != address(0), "invalid address");
         if (_flag && !signers[_signer]) {
             totalSigner++;
         } else if (!_flag && signers[_signer]) {
@@ -218,13 +218,13 @@ contract XOracleMessage is OwnableUpgradeable, PausableUpgradeable {
     }
 
     function setThreshold(uint256 _threshold) external onlyOwner {
-        require(_threshold > 0 && _threshold <= totalSigner, "threshold invalid");
+        require(_threshold > 0 && _threshold <= totalSigner, "invalid threshold");
         threshold = _threshold;
         emit SetThreshold(_threshold);
     }
 
     function setWhitelist(address _whitelist, bool _flag) external onlyOwner {
-        require(_whitelist != address(0), "address invalid");
+        require(_whitelist != address(0), "invalid address");
         whitelists[_whitelist] = _flag;
         emit SetWhitelist(_whitelist, _flag);
     }
